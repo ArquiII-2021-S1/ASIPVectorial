bad3fa3437ab25de84f82c8d054a4987f0c52300
Juan Navarro <jnavcamacho@gmail.com>
2021-05-29 15:08:14 -0600

    updated mif and image creation

--- utils/imageUtils.py
+++ utils/imageUtils.py
@@ -4,14 +4,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys, getopt
 
-# Esta funcion es usada para tomar dump file y crear un arreglo de valores de pixeles.
-def getPixelArrayFromFile(filename):
+from numpy.lib.arraysetops import unique
+
+def getPixelArrayFromDumpFile(filename):
     #fileInput = open(filename, "r")
     with open(filename, "r") as fileInput:
         lines = []
         for line in fileInput:
             #print(line.split(':')[1])
             lines.append(int(line.split(':')[1]))
+    return lines
+
+# Esta funcion es usada para tomar dump file y crear un arreglo de valores de pixeles.
+def getArrayFromFile(filename):
+    #fileInput = open(filename, "r")
+    with open(filename, "r") as fileInput:
+        lines = []
+        for line in fileInput:
+            #print(line.split(':')[1])
+            lines.append(int(line))
     return lines
 
 def writeImageMif(fileName, pixels):
@@ -45,8 +56,6 @@ def generateMifChannels(filename):
     width, height = image.size
     pixels = list(image.getdata())
 
-    plotImage(pixels, 200)
-
     channelRed = []
     channelGreen = []
     channelBlue = []
@@ -63,10 +72,12 @@ def generateMifChannels(filename):
     print("\033[0m")
 
 
-def plotImage(pixelArray, rows):
-    matrix = np.resize(pixelArray, (rows, len(pixelArray) // rows))
+def plotImage(pixelArray, rows,cols):
+    matrix = np.array(pixelArray).reshape(rows,cols)
     plt.subplot()
     plt.imshow(matrix)
+    print(len(matrix[0]))
+    print(matrix[0][0:10])
     plt.savefig("./output/generated.png")
     plt.show()
     
@@ -76,24 +87,78 @@ def show():
     channelGreen = getPixelArrayFromFile("./input/dumpGreen.txt") 
     channelBlue = getPixelArrayFromFile("./input/dumpBlue.txt")
     unifiedImagePixels = []
-    for i in range (0, (200*200) -1):
-        unifiedImagePixels.append( [(channelRed[i],channelGreen[i],channelBlue[i])])
-    plotImage(unifiedImagePixels,200)
+    for i in range (0, (200*200)):
+        #channelRed[i]/255,channelGreen[i]/255,channelBlue[i]/255)
+        pixel = (channelRed[i],channelGreen[i],channelBlue[i])
+        unifiedImagePixels.append( pixel )
+    print(unifiedImagePixels)
+    plotImage(unifiedImagePixels,200,200)
     
+def populateROM(imageFile, gradientFilename, customGradientFileName, filename1,filename2):
+    image = Image.open(imageFile, "r")
+    width, height = image.size
+    pixels = list(image.getdata())
+
+    channelRed = []
+    channelGreen = []
+    channelBlue = []
+    for item in range(0, len(pixels) ):
+        channelRed.append(pixels[item][0]) # channel red
+        channelGreen.append(pixels[item][1])  # channel green
+        channelBlue.append(pixels[item][2]) # channel blue
+    print(width)
+    print(height)
 
 
-def main(argv):
-    opts, args = getopt.getopt(argv, "i:s:d:", ["inputFile", "show" , "debug"])
-    for opt, arg in opts:
-        if opt == '-i':
-            # "inputImage.png"
-            print(arg)
-            generateMifChannels(arg)
-        else:
-            show()
+    gradientArray = getArrayFromFile(gradientFilename)
+    customGradientArray =getArrayFromFile(customGradientFileName)
+
+    depth = 2 ** 16
+    filePart1 = open(filename1, "w")
+    header = """DEPTH = {}; -- The size of memory in words
+    WIDTH = 8; -- The size of data in bits
+    ADDRESS_RADIX = DEC; -- The radix for address values
+    DATA_RADIX = DEC; -- The radix for data values
+    CONTENT -- start of (address : data pairs)
+    BEGIN \n""".format(
+        depth
+    )
+    filePart1.write(header + "\n")
+
+    for i in range (0 , len(gradientArray)): # 0 - 199
+        filePart1.write(str(i) + " : " + str(gradientArray[i]) + ";\n")
     
+    for j in range (0 , len(customGradientArray)) : # 200 - 399 
+        filePart1.write(str(j+200) + " : " + str(customGradientArray[j]) + ";\n")
+    
+    for r in range (0,len(channelRed)):
+        filePart1.write(str(r+400) + " : " + str(channelRed[r]) + ";\n")
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
+    for g in range (0,25136):
+        filePart1.write(str(g+40400) + " : " + str(channelGreen[g]) + ";\n")
+    filePart1.write("END;" + "\n")
+    
+    filePart2 = open(filename2, "w")
+    header = """DEPTH = {}; -- The size of memory in words
+    WIDTH = 8; -- The size of data in bits
+    ADDRESS_RADIX = DEC; -- The radix for address values
+    DATA_RADIX = DEC; -- The radix for data values
+    CONTENT -- start of (address : data pairs)
+    BEGIN \n""".format(
+        depth
+    )
+    filePart2.write(header + "\n")
+
+    print(len(channelGreen))
+    for g in range (0,25136):
+        filePart2.write(str(g) + " : " + str(channelGreen[g+14864]) + ";\n")
+    
+    for b in range (0,len(channelBlue)):
+        filePart2.write(str(b+25136) + " : " + str(channelBlue[b]) + ";\n")
+    filePart2.write("END;" + "\n")
 
 
+
+
+
+populateROM("./input/test.png", "./input/gradient.txt", "./input/customGradient.txt","./output/rom1.mif","./output/rom2.mif")
