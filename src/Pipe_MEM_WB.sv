7e89a31808c27a3fbb07ffb2151bc6859ff80ac9
Gustavo Segura <yo@gustavosegura.com>
2021-05-30 04:27:34 -0600

    pipe MEM WB

--- src/Pipe_MEM_WB.sv
+++ src/Pipe_MEM_WB.sv
@@ -1,55 +1,67 @@
-module Pipe_MEM_WB #(N = 32)
-                    (CLK,
-                     RST,
-                     ReadData_i,
-                     RF_WE_i,
-                     MemWE_i,
-                     WBSelect_i,
-                     AluResult_i,
-                     A3_i,
-                     ReadData_o,
-                     RF_WE_o,
-                     MemWE_o,
-                     WBSelect_o,
-                     AluResult_o,
-                     A3_o);
-    
-    
-    input logic CLK, RST;
-    input logic [N-1:0] ReadData_i,AluResult_i;
-    input logic MemWE_i,WBSelect_i,RF_WE_i;
-    input logic [3:0] A3_i;
-    
-    
-    //logic [N-1:0] ReadData,AluResult ;
-    //logic MemWE,WBSelect,RF_WE;
-    //logic [3:0] A3;
-    
-    
-    output logic [N-1:0] ReadData_o,AluResult_o ;
-    output logic MemWE_o,WBSelect_o,RF_WE_o;
-    output logic [3:0] A3_o;
-    
-    
-    always @(posedge CLK)
-        if (RST)
-        begin
-            AluResult_o <= 0;
-            ReadData_o  <= 0;
-            RF_WE_o     <= 0;
-            MemWE_o     <= 0;
-            WBSelect_o  <= 0;
-            A3_o        <= 0;
-        end
-    
-    else
-    begin
-    ReadData_o  <= ReadData_i;
-    RF_WE_o     <= RF_WE_i;
-    MemWE_o     <= MemWE_i;
-    WBSelect_o  <= WBSelect_i;
-    A3_o        <= A3_i;
-    AluResult_o <= AluResult_i;
+module Pipe_MEM_WB #(
+    N = 32,
+    V = 20
+) (
+    CLK,
+    RST,
+    enable_i,
+
+    A3_i,
+    Data_Mem_S_i,
+    Data_Mem_V_i,
+    Data_Result_S_i,
+    Data_Result_V_i,
+    OpType_i,
+    RegFile_WE_i,
+    WBSelect_i,
+
+    A3_o,
+    Data_Mem_S_o,
+    Data_Mem_V_o,
+    Data_Result_S_o,
+    Data_Result_V_o,
+    OpType_o,
+    RegFile_WE_o,
+    WBSelect_o
+
+);
+
+
+  input logic CLK, RST, enable_i;
+
+  input logic [N-1:0] Data_Mem_S_i, Data_Result_S_i;
+  input logic RegFile_WE_i, WBSelect_i;
+  input logic [3:0] A3_i;
+  input logic [1:0] OpType_i;
+  input logic [V-1:0][N-1:0] Data_Mem_V_i, Data_Result_V_i;
+
+
+  output logic [N-1:0] Data_Mem_S_o, Data_Result_S_o;
+  output logic RegFile_WE_o, WBSelect_o;
+  output logic [3:0] A3_o;
+  output logic [1:0] OpType_o;
+  output logic [V-1:0][N-1:0] Data_Mem_V_o, Data_Result_V_o;
+
+  always @(posedge CLK)
+    if (RST) begin
+      A3_o <= 0;
+      Data_Mem_S_o <= 0;
+      Data_Mem_V_o <= 0;
+      Data_Result_S_o <= 0;
+      Data_Result_V_o <= 0;
+      OpType_o <= 0;
+      RegFile_WE_o <= 0;
+      WBSelect_o <= 0;
+    end else begin
+      if (enable_i) begin
+        A3_o <= A3_i;
+        Data_Mem_S_o <= Data_Mem_S_i;
+        Data_Mem_V_o <= Data_Mem_V_i;
+        Data_Result_S_o <= Data_Result_S_i;
+        Data_Result_V_o <= Data_Result_V_i;
+        OpType_o <= OpType_i;
+        RegFile_WE_o <= RegFile_WE_i;
+        WBSelect_o <= WBSelect_i;
+      end
     end
-    
 endmodule
