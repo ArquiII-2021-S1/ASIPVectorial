d5b176a44eeb07d4c45488b31eb03effb7371063
Gustavo Segura <yo@gustavosegura.com>
2021-06-02 00:37:04 -0600

    testbech cpu

--- src/CPU_tb.sv
+++ src/CPU_tb.sv
@@ -22,28 +22,28 @@ module CPU_tb();
   logic [31:0] pos =0;
 CPU cpu (	
 							.CLK(CLK),.RST(RST), 
-							.inst_mem_data_i(inst_mem_data), 
-							.inst_mem_address_o(inst_mem_address), 
-							.data_mem_out_data_i(data_mem_out_data), 
-							.data_mem_address_o(data_mem_address),
-							.data_mem_in_data_o(data_mem_in_data), 
-							.data_mem_WE_o(data_mem_WE));
+							.inst_mem_data_i(), 
+							.inst_mem_address_o(), 
+							.data_mem_out_data_i(), 
+							.data_mem_address_o(),
+							.data_mem_in_data_o(), 
+							.data_mem_WE_o());
 
 
-Inst_ROM instROM (
-							.address(inst_mem_address[11:0]),
-							.clock(CLK_ng),
-							.q(inst_mem_data));
+// Inst_ROM instROM (
+// 							.address(inst_mem_address[11:0]),
+// 							.clock(CLK_ng),
+// 							.q(inst_mem_data));
 
 
-DataMemoryManager    dataMemoryManager(	
-							.address_i(data_mem_address), 
-							.CLK(CLK), 
-							.data_i(data_mem_in_data), 
-							.wren_i(data_mem_WE),
-							.button_i(button),
-							.data_o(data_mem_out_data), 
-							.LEDs_o(LEDs));
+// DataMemoryManager    dataMemoryManager(	
+// 							.address_i(data_mem_address), 
+// 							.CLK(CLK), 
+// 							.data_i(data_mem_in_data), 
+// 							.wren_i(data_mem_WE),
+// 							.button_i(button),
+// 							.data_o(data_mem_out_data), 
+// 							.LEDs_o(LEDs));
 							
 							
 
